145e3af6b83a794edacb23fded2908f754075d24
Rich Lobo <lobo.r0018@gmail.com>
2025-09-30 17:18:59 +0800

    Fix error after regiusttration

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -53,7 +53,11 @@ router.post('/register', async(req, res) => {
         });
 
         await user.save();
-        res.status(200).json({ message: 'User registered successfully. Please wait for admin approval' });
+
+        const token = generateToken(user._id);
+
+        return res.status(201).json({message: 'User created successfully. Please wait for admin approval.'})
+        
    } catch (error) {
         console.log("Error on the register route", error);
         res.status(500).json({ message: 'Internal server error' });
